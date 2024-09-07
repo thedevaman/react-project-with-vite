b1f26f8f55853729faaad346b5514945791839eb
unknown <devaman2508@gmail.com>
2024-09-07 07:52:22 +0530

    useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
 import {useState} from 'react'
+import {useRef} from 'react'
 const App = ()=>{
 
-  const [count,setcount]=useState(0)
-
+  const title = useRef(null)
+  const [color,setColor]=useState('black')
+ const test = ()=>{
+  setColor(color=='black'?'red':'black')
+ }
+ const reftest = ()=>{
+  title.current.style.color= "blue"
+  title.current.style.fontWeight= "bold"
+ }
 
 return(
   <div>
-  <h1>vite</h1>
-  <button onClick={()=>setcount(count+1)}>
-    count -{count}
-  </button>
+  <h1 ref={title} style={{color:color}}>useRef Effect</h1>
+  <button onClick={test}>Test useState</button>
+  <button onClick={reftest}>Test useRef</button>
+  <div style={{height:'100vh',background:'red',  display:'flex',
+      alignItems:'center',
+      justifyItems:'center'}}>
+    <div style={
+      {width:350,
+      height:280,
+      background:'blue',
+    
+      }}>
+      
+    </div>
+  </div>
   </div>
 )
 }
